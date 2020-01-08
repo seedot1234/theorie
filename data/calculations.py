0a1c3cf01483a34992e1b3e2ff36b72d33f436e7
Sarah-Jane <sj.vanels@tulsie.nl>
2020-01-08 14:49:50 +0100

    created file for calculations space time continuum

---
+++ data/calculations.py
@@ -0,0 +1,13 @@
+""""
+calculations.py 
+
+In this file, you'll find the all calculations made for calculating the amount of 
+possible outcomes 
+
+8/1/2020
+
+""""
+import pandas as pd
+
+# calculate the station with the most amount of connections 
+
