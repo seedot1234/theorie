a4592aa19e3016cfac658a58d21366a5bdea2f9b
unknown <susan.vandenbroek@student.uva.nl>
2020-01-09 13:34:06 +0100

    commit to pull

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from code1.classes import connection, route, station
+from code1.classes.route import Route
 from random import randrange
 import csv, io, os
 
@@ -10,30 +11,29 @@ connections_list = []
 for station in stations_objects:
     print(station)
 
+# def create_connection():
+# create connection objects
+connection_objects = []
 
-def create_connection():
-    # create connection objects
-    connection_objects = []
+# loop over all connections
+for connection in connections_list:
+    time = int(connection[-1])
 
-    # loop over all connections
-    for connection in connections_list:
-        time = int(connection[-1])
+    # loop over all station objects
+    for station in stations_objects:
 
-        # loop over all station objects
-        for station in stations_objects:
-
-            # if the object is the same as the station in the connection, save it
-            if station.name == connection[0]:
-                station_a = station
-            if station.name == connection[1]:
-                station_b = station
+        # if the object is the same as the station in the connection, save it
+        if station.name == connection[0]:
+            station_a = station
+        if station.name == connection[1]:
+            station_b = station
+    
+    # create connection object
+    connection_object = Connection(station_a, station_b, time)
+    
+    # save connection object in a list
+    connection_objects.append(connection_object)
         
-        # create connection object
-        connection_object = Connection(station_a, station_b, time)
-        
-        # save connection object in a list
-        connection_objects.append(connection_object)
-
 
 
 # add connections to stations
@@ -43,19 +43,25 @@ for station in stations_objects:
     for connection in connection_objects:
         # if station == connection.station_a or station == connection.station_b:
         #     station.add_connection(connection)
-
+        print("###")
+        print(station)
+        print(connection.station_a)
+        print(connection.station_b)
+        print("###")
 
         # if the station is in the connection object, add its corresponding connected station
         if station == connection.station_a:
             station.add_connection(connection.station_b, connection.time)
         if station == connection.station_b:
             station.add_connection(connection.station_a, connection.time)
+    # print("###")
+    # print(station.connections)
+    # print("###")
 
 
 
 # set first station randomly
 current_station = stations_objects[randrange(len(stations_objects))]
-
 # start new route
 route = Route(1, current_station)
 
