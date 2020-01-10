b28dc1f6c6298441f7523a3291b8b0300fea4d47
unknown <susan.vandenbroek@student.uva.nl>
2020-01-10 11:27:02 +0100

    comments added

--- main.py
+++ main.py
@@ -4,28 +4,19 @@ from random import randrange
 import random
 import csv, io, os
 
-
+# create station objects from csv
 station_csv = os.path.join("data", "ConnectiesHolland.csv")
 station_objects = load_data.create_station_list(station_csv)
 
+# create connection objects from csv
 connection_csv = os.path.join("data", "ConnectiesHolland.csv")
 connection_objects = load_data.create_connection(connection_csv, station_objects)
 
+# add connections to stations
 connections_list = []   
-
 load_data.add_station_connection(station_objects, connection_objects)
-for station in station_objects:
-    print(station.connections)
 
-for connection in connection_objects:
-    print(connection)
-# print(len(connection_objects))
-
-
-
-# make new routes until all connections have been used
-
-
+# make new routes randomly until all connections have been used
 def random_solution():
     while True:
         visited_connections = []
