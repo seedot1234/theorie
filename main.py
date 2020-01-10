7ca028134b95badc35a7b5e85ac5342727769905
unknown <susan.vandenbroek@student.uva.nl>
2020-01-10 12:02:40 +0100

    nieuwe comments

--- main.py
+++ main.py
@@ -1,37 +1,37 @@
-""""
-main.py 
+"""
+main.py
 
 Calls all functions in the repository 'theorie'
 
 10/1/20
 
-""""
+"""
 from code1.classes import connection, route, station, load_data
 from code1.classes.route import Route
 from random import randrange
 import random
 import csv, io, os
 
-<<<<<<< HEAD
-# create station objects from csv
-=======
->>>>>>> 05901d2c55a42860b7e9927304a8e5e5f0a02f52
+# creates station objects from csv
 station_csv = os.path.join("data", "ConnectiesHolland.csv")
 station_objects = load_data.create_station_list(station_csv)
 
-# create connection objects from csv
+# creates connection objects from csv
 connection_csv = os.path.join("data", "ConnectiesHolland.csv")
 connection_objects = load_data.create_connection(connection_csv, station_objects)
 
-# add connections to stations
-connections_list = []   
+# adds connections to stations
+connections_list = []
 load_data.add_station_connection(station_objects, connection_objects)
 
-# make new routes randomly until all connections have been used
+# makes new routes randomly until all connections have been used
 def random_solution():
     while True:
         visited_connections = []
         total_time = 0
+        print("="*100)
+
+        # hier moet een lijst komen die de lijnvoering bijhoudt (lijst van Route objecten)
         for total_routes in range (7):
 
             # return statement that checks whether all connections have been used yet
@@ -39,27 +39,43 @@ def random_solution():
             #     print(total_time)
             #     return True
 
-            # set starting station randomly
+            # sets starting station randomly
             current_station = station_objects[randrange(len(station_objects))]
 
-            # start new route
+            # starts new route
             route = Route(total_routes, current_station)
 
-            # keep adding stations
+
+            # maak dit een gedefineerde while statement met een conditie ipv while True
+            # keeps on adding stations until maximum time has been reached 
             while True:
                 
                 # putting it here makes it shorter..?
                 if len(connection_objects) == len(visited_connections):
                     print(total_time)
-                    return True
+                    # print hier de lijnvoering
+                    return 'Finished'
 
-                # pick a random new station out of all connections of the current station
+                # vraag alle connecties op van current station
+
+                # connecties van station sorteren op tijd 
+
+                # kies station met korste afstand
+
+                # maak dit station het nieuwe station ipv random choice
+
+                # picks a random new station out of all connections of the current station
                 new_station = random.choice(list(current_station.connections.keys()))
+
+                
+                # finds the time for the new station 
                 time = int(current_station.connections[new_station])
                 
-                # stop adding stations until the total time would exceed 120 minutes
+                # stops adding stations until the total time would exceed 120 minutes
                 if time + route.total_time > 120:
                     total_time += route.total_time
+
+                    # voeg Route toe aan lijnvoering lijst
                     break
                 
                 # add a new station to the route
