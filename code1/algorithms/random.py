85c24e54e8d1a4a2f6a936248365d656b73303e5
LinzaH <linza.hitija@gmail.com>
2020-01-10 12:05:32 +0100

    Put random algorithm in seperate file

---
+++ code1/algorithms/random.py
@@ -0,0 +1,60 @@
+
+"""
+random.py
+
+@author Heuristic Heroes
+@version
+
+random algorithm
+"""
+
+def random_solution():
+    while True:
+        visited_connections = []
+        total_time = 0
+        for total_routes in range (7):
+
+            # return statement that checks whether all connections have been used yet
+            # if len(connection_objects) == len(visited_connections):
+            #     print(total_time)
+            #     return True
+
+            # set starting station randomly
+            current_station = station_objects[randrange(len(station_objects))]
+
+            # start new route
+            route = Route(total_routes, current_station)
+
+            # keep adding stations
+            while True:
+                
+                # putting it here makes it shorter..?
+                if len(connection_objects) == len(visited_connections):
+                    print(total_time)
+                    return True
+
+                # pick a random new station out of all connections of the current station
+                new_station = random.choice(list(current_station.connections.keys()))
+                time = int(current_station.connections[new_station])
+                
+                # stop adding stations until the total time would exceed 120 minutes
+                if time + route.total_time > 120:
+                    total_time += route.total_time
+                    break
+                
+                # add a new station to the route
+                route.add_station(new_station, time)
+
+                # find the connection that was added
+                for connection in connection_objects:
+                    if (connection.station_a == current_station and connection.station_b == new_station) or (connection.station_a == new_station and connection.station_b == current_station):
+                        
+                        # if the connection wasn't used before, add it to the visited connections list
+                        if connection in visited_connections:
+                            break
+                        visited_connections.append(connection)
+                
+                # set this new station as the current station
+                current_station = new_station
+            
+            print(route)
